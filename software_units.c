7ee138f5db3e2611da5b22a2849138a789bbb8e6
ANOLD <nairobianmerchant254@gmail.com>
2025-09-22 21:48:15 +0300

    Add files via upload

---
+++ software_units.c
@@ -0,0 +1,22 @@
+/*
+Name:ANOLD MATONDA
+REG NO:PA106/G/28787/25
+*/
+
+#include <stdio.h>
+
+int main(){
+
+printf("1:discrete mathematics \n");
+printf("2:Mathematics For Science\n");
+printf("3:Fundamentals of ICT\n");
+printf("4:Computer System and Organisation\n");
+printf("5:Communication skills\n");
+printf("6:Introduction to Programming And Problem Solving\n");
+printf("7:Fundamentals of System Analysis & Design\n");
+
+return 0;
+}
+
+
+
