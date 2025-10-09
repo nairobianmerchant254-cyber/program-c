23d6d698e89a998b4a2b4fd083ec53cb6dc90222
ANOLD <nairobianmerchant254@gmail.com>
2025-10-09 09:27:28 -0700

    Add files via upload

---
+++ DIGITS.C
@@ -0,0 +1,14 @@
+/*
+NAME:OBAGA ANOLD MATONDA
+REG NO:PA106/G/28787/25
+DESCRIPTION:PRINTING NUMBERS FROM 100-50 USING FOR LOOP
+*/
+
+#include<stdio.h>
+int main(){
+int i;
+for(i=100;i>=50;i--)
+printf("%d\n",i);
+return 0;
+}
+    
