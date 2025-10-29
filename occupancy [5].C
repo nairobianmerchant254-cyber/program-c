c66046afc3bed540aac9353a8d99959ce978998e
ANOLD <nairobianmerchant254@gmail.com>
2025-10-29 11:08:57 +0300

    Add files via upload

---
+++ occupancy [5].C
@@ -0,0 +1,27 @@
+/*
+NAME:ANOLD OBAGA MATONDA
+REG NO:PA106/G/28787/25
+DESCRIPTION:Displaying 2D Array
+*/
+
+#include <stdio.h>
+
+int main(){
+
+int i,j;
+int occupance[5][10]={
+{1,1,1,0,0,0,1,0,1,1},
+{1,1,1,0,0,0,1,0,1,1},
+{1,1,1,0,0,0,1,0,1,1},
+{1,1,1,0,0,0,1,0,1,1},
+{1,1,1,0,0,0,1,0,1,1}
+};
+
+for(i=0;i<5;i++){
+for(j=0;j<10;j++){
+printf("%d\t",occupance[i][j]);
+}
+printf("\n");
+}
+return 0;
+}
